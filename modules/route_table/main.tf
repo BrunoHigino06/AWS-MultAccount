d3d2282367027ffc663ab5ce36bd4bce34efbd75
Bruno Higino Souza <bruno.higino.d.souza@accenture.com>
2023-08-21 17:33:04 -0300

    21.08.2023

--- modules/route_table/main.tf
+++ modules/route_table/main.tf
@@ -1,1 +1,26 @@
 # route table resource
+resource "aws_route_table" "route_table" {
+  count            = length(var.route_table)
+  vpc_id           = var.route_table[count.index].vpc_id
+  propagating_vgws = var.route_table[count.index].propagating_vgws
+  dynamic "route" {
+    for_each = var.route_table
+    content {
+      cidr_block                = route.value.cidr_block
+      gateway_id                = route.value.gateway_id
+      nat_gateway_id            = route.value.nat_gateway_id
+      carrier_gateway_id        = route.value.carrier_gateway_id
+      core_network_arn          = route.value.core_network_arn
+      egress_only_gateway_id    = route.value.egress_only_gateway_id
+      local_gateway_id          = route.value.local_gateway_id
+      network_interface_id      = route.value.network_interface_id
+      transit_gateway_id        = route.value.transit_gateway_id
+      vpc_endpoint_id           = route.value.vpc_endpoint_id
+      vpc_peering_connection_id = route.value.vpc_peering_connection_id
+    }
+
+  }
+  tags = merge(var.route_table[count.index].tags,
+    {Name = var.route_table[count.index].name}
+  )
+}
--- modules/route_table/variables.tf
+++ modules/route_table/variables.tf
@@ -1,7 +1,23 @@
 # route table vars
 variable "route_table" {
-  type                                             = list(object({
-    name                                           = string
+  type    = list(object({
+    name   = string
+    vpc_id = string
+    propagating_vgws = optional(string)
+    route            = map(object({
+      cidr_block                = string
+      gateway_id                = optional(string)
+      nat_gateway_id            = optional(string)
+      carrier_gateway_id        = optional(string)
+      core_network_arn          = optional(string)
+      egress_only_gateway_id    = optional(string)
+      local_gateway_id          = optional(string)
+      network_interface_id      = optional(string)
+      transit_gateway_id        = optional(string)
+      vpc_endpoint_id           = optional(string)
+      vpc_peering_connection_id = optional(string)
+    }))
+    tags   = map(string)
     
     }))
     description = "route table variables"
