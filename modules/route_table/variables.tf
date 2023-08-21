f6a4a99deefde47aabc836986750763ebfb70e90
Bruno Higino Souza <bruno.higino.d.souza@accenture.com>
2023-08-21 17:34:02 -0300

    Update variables.tf

--- modules/route_table/variables.tf
+++ modules/route_table/variables.tf
@@ -1,10 +1,10 @@
 # route table vars
 variable "route_table" {
-  type    = list(object({
-    name   = string
-    vpc_id = string
-    propagating_vgws = optional(string)
-    route            = map(object({
+  type                          = list(object({
+    name                        = string
+    vpc_id                      = string
+    propagating_vgws            = optional(string)
+    route                       = map(object({
       cidr_block                = string
       gateway_id                = optional(string)
       nat_gateway_id            = optional(string)
@@ -17,9 +17,9 @@ variable "route_table" {
       vpc_endpoint_id           = optional(string)
       vpc_peering_connection_id = optional(string)
     }))
-    tags   = map(string)
+    tags                        = map(string)
     
     }))
-    description = "route table variables"
+    description                 = "route table variables"
     default = null
 }
